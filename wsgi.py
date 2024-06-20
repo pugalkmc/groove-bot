cc4b6deefbefa68aa18a88f1fd567f0ea666a7a0
pugalkmc <pugalkmc@gmail.com>
2024-06-20 09:35:24 +0530

    deploy changes

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ import uvicorn
 from main import main
 
 if __name__ == "__main__":
-    uvicorn.run(main(), host="0.0.0.0", port=int(os.getenv("PORT", 8443)))
+    uvicorn.run(main(), host="0.0.0.0", port=int(os.getenv("PORT", default=8443)))
